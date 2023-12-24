7525389ca5c792347e6112b87c8713685d7e1dd3
Menna-Allah Hamouda <33626317+menna-ham@users.noreply.github.com>
2023-12-24 12:25:50 +0000

    Update 8 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google/index.js";
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,19 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
 export default function page() {
+  let router = useRouter();
+  let handleClick = () => {
+    console.log("routing to home");
+    router.replace("/");
+  };
   return (
     <div>
-      <Link href="/"> Home</Link>
+      {/* <Link href="/"> Home</Link> */}
+      <button onClick={handleClick} className="bg-green-400 p-2 my-5">
+        Back to Home Page{" "}
+      </button>
       <h1> Products List</h1>
       <h3>
         {/* replace:replacing the current state instead of adding new URl to the stack */}
