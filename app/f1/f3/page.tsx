7c7243052d238211a8cee7c01b84ee3259a5d774
Menna-Allah Hamouda <33626317+menna-ham@users.noreply.github.com>
2023-12-30 12:42:30 +0000

    Update 3 files

--- app/f1/f3/page.tsx
+++ app/f1/f3/page.tsx
@@ -4,7 +4,6 @@ export default function F3Page() {
   return (
     <>
       <h1>F3 Page </h1>
-      <Link href=""> </Link>
     </>
   );
 }
---
+++ app/f1/f4/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function F4Page() {
+  return (
+    <>
+      <h1>F4 Page </h1>
+      <Link href="/f1/f4">F4 </Link>
+    </>
+  );
+}
